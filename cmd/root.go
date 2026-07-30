b9cdfd14471498138772b5b8117c7dd822c147f9
Leon Li <leon.nl0926@gmail.com>
2026-07-30 17:59:32 +0900

    added cfg file initialization

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,13 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -45,4 +49,52 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig(cmd *cobra.Command) error {
+	// allow viper to use env vars
+	viper.SetEnvPrefix("cobruh")
+	// allow for nested key in env vars, e.g. COBRUH_SERVER_PORT=8080
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "*", "-", "*"))
+	viper.AutomaticEnv()
+
+	// handle config file
+	if cfgFile != "" {
+		// use config file from flag
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// find home directory elsewhere
+		home, err := os.UserHomeDir()
+		// panic if we can't find home dir
+		cobra.CheckErr(err)
+
+		// search for config in home dir with name ".cobruh" (w/o extensions)
+		viper.AddConfigPath(".")
+		viper.AddConfigPath(home + "./cobruh")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("config")
+	}
+
+	// read config file if found
+	// use robust error check to handle file not found errors, panic on other errors
+	if err := viper.ReadInConfig(); err != nil {
+		// its ok if config file does not exist
+		var configFileNotFound viper.ConfigFileNotFoundError
+		if !errors.As(err, &configFileNotFound) {
+			return err
+		}
+	}
+
+	// bind cobra flags to viper
+	// makes flag values available to through viper
+	// flag set of the command that is passed in is binded
+	err := viper.BindPFlags(cmd.Flags())
+	if err != nil {
+		return err
+	}
+
+	//useful debugging info
+	fmt.Println("Using config file:", viper.ConfigFileUsed())
+
+	return nil
 }
